d2becb89b66ef03d1bdc1f6a55f189b09b35bc46
Dalila K Leo <dalilakatialeo@gmail.com>
2021-10-28 13:33:03 +1000

    Update header.php

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>LPA_ecomms Dalila</title>
+    <title>LPA</title>
     <link href="js/jquery-ui-1.12.1.custom/jquery-ui.min.css" rel="stylesheet" type="text/css">
     <link href="css/style.css" rel="stylesheet" type="text/css">
     <script src="js/jquery-3.1.1.min.js" type="text/javascript"></script>
@@ -14,7 +14,7 @@
   <body>
     <div id="header">
       <div style="margin-left: 10px">
-        LPA ecomms website
+        Logic Peripherals Australia (LPA)
       </div>
     </div>
     <div style="float:right"><?php echo $displayName; ?></div>
